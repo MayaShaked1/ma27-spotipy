1d44e571eba16118b9f20f59d926def3a466a42b
MayaShaked1 <shakedmaya1@gmail.com>
2022-02-10 15:55:03 +0200

    design of user type

---
+++ music/user_type/access_to info_of_each.py
@@ -0,0 +1,16 @@
+class FreeUser:
+    def limit_playlists(self):
+        return 0
+
+    def limit_searching(self):
+        return 0
+
+
+class PremiumUSer:
+    def info(self):
+        return 0
+
+
+class Artist:
+    def info(self):
+        return 0

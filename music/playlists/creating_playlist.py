581672818da99907fd1f3c35724c1c11557ea6d8
MayaShaked1 <shakedmaya1@gmail.com>
2022-02-10 15:47:44 +0200

    playlists and logger design

--- music/playlists/creating_playlist.py
+++ music/playlists/creating_playlist.py
@@ -2,3 +2,5 @@ import uuid
 uuid.uuid4()#unique name for the playlist
 
 #{user_id:[{playlist_id:[songs_list]},{playlist_id:[songs_list]},{playlist_id:[songs_list]}]}
+
+#the info will be written to the file info_about_users.json
